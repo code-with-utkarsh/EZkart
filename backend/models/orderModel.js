a89a0c64de416d899fe97649db680000de4a5a8d
Utkarsh <utkarshk6285@gmail.com>
2024-05-05 00:36:55 +0530

    added

---
+++ backend/models/orderModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        product: {
+          type: mongoose.ObjectId,
+          ref: "Products",
+        },
+        count: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+    payment: {},
+    buyer: {
+      type: mongoose.ObjectId,
+      ref: "users",
+    },
+    status: {
+      type: String,
+      default: "Not Process",
+      enum: ["Not Process", "Processing", "Shipped", "delivered", "cancel"],
+    },
+    // total: {
+    //   type: Number,
+    //   default: 0,
+    // },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Order", orderSchema);
---
+++ backend/models/productModel.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+import Review from "./reviewModel.js";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: mongoose.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+    shipping: {
+      type: Boolean,
+    },
+    reviews: [
+      {
+        type: mongoose.ObjectId,
+        ref: "Review",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+//delete all reviews when product is deleted
+productSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
+});
+
+export default mongoose.model("Products", productSchema);
